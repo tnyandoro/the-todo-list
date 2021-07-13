698b3d15353a977a181f7fc5b7ceef209e18cc56
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-13 16:24:34 +0200

    Add a function to select item to an array

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import './style.css';
-import printMe from './print.js';
+// import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+// The array that holds the listed items
+let todoItems = [];
 
-  element.innerHTML = `${"Hello"}, ${"Webpack"}`;
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
+function addTodo(item) {
+  const todo = {
+    item,
+    checked: false,
+    id: Date.now(),
+  };
+todoItems.push(todo);
+console.log(todoItems);
 }
 
-document.body.appendChild(component());
+// lets get the form element
+const form = document.querySelector('.todo-form');
+// Lets add an event listener to submit the form
+form.addEventListener('submit', event => {
+  // stop the page from refreshing on submit
+  event.preventDefault();
+  // pick the form input element
+  const input = document.querySelector('.todo-input');
+
+  // Lets get the input values
+  const item = input.value.trim();
+  if(item !== '') {
+    addTodo(item);
+    input.value = '';
+  }
+});
