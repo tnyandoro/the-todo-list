bd983eca75bed01c14085b5687b8601f79d341be
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-13 12:21:29 +0200

    Add the export function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import './style.css';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
   element.innerHTML = `${"Hello"}, ${"Webpack"}`;
   element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,13 @@ module.exports = {
   },
   module: {
     rules: [{
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    }, ],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 }
