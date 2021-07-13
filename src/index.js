203efc5e0d52957e3463e163f60e2d927800699e
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-14 00:27:15 +0200

    Declare the input, button and the todolist arrays

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Selectors for
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Events Listeners 
+
+// Functions 
