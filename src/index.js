45f3e01de12b10caffe2586b0d0751f6451c36b3
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-13 18:49:59 +0200

    Lets add a function to check the items

--- src/index.js
+++ src/index.js
@@ -9,10 +9,19 @@ function addTodo(item) {
     checked: false,
     id: Date.now(),
   };
-todoItems.push(todo);
-console.log(todoItems);
+  todoItems.push(todo);
+  renderTodo(todo);
 }
 
+function toggleDone(key) {
+  // lets find Index is an array method that will return the position of an array item in the array
+  const index = todoItems.findIndex(item => item.id === Number(key));
+  // Lets locate the todo item in the todoitems array and set its checked property to the opposite of
+  // meaning true will bcome false and rVksvsnNCdJ
+  todoItems[index].checked = !todoItems[index].checked;
+  renderTodo(todoItems[index]);
+
+}
 // lets get the form element
 const form = document.querySelector('.todo-form');
 // Lets add an event listener to submit the form
@@ -24,7 +33,7 @@ form.addEventListener('submit', event => {
 
   // Lets get the input values
   const item = input.value.trim();
-  if(item !== '') {
+  if (item !== '') {
     addTodo(item);
     input.value = '';
   }
@@ -32,25 +41,35 @@ form.addEventListener('submit', event => {
 
 function renderTodo(todo) {
   // Lets get 1st element with a class of lodo
-  const list = document.querySelector('.todos-list')
+  const list = document.querySelector('.todo-list')
 
   // Using the ternary operator to if 'todo.checked' is
   // then assign done else empty
-const isChecked = todo.checked ? 'done': '';
-// Lets create an 'li' element and assigned it to the'node' 
-const node = document.createElement('li')
-// setting the class attribute to 'todo
-node.setAttribute('class', `todo-item ${isChecked}`);
-// Lets set the data-key attribute to the id of the todo 
-node.setAttribute('data-key', todo.id);
-// Set the contents of the `li` element we have created above
-node.innerHTML = `
-<input id="${todo.id}" type="checkbox"/>
-<label for="${todo.id}" class="tick do-tick"></label>
-<span>${todo.item}</span>
-<button class="delete-todo btn-danger">
-<svg><use href="#delete-icon"></use></svg>
-</button>`;
-// Lets Append the element to the Dom of the last child to the element by the list variable 
-list.append(node);
+  const isChecked = todo.checked ? 'done' : '';
+  // Create an `li` element and assign it to `node`
+  const node = document.createElement("li");
+  // Set the class attribute
+  node.setAttribute('class', `todo-item ${isChecked}`);
+  // Set the data-key attribute to the id of the todo
+  node.setAttribute('data-key', todo.id);
+  // Set the contents of the `li` element created above
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox" form-check-input/>
+    <label for="${todo.id}" class="tick form-check-labe"></label>
+    <span>${todo.text}</span>
+    <button class="button ">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+  // Lets Append the element to the Dom of the last child to the element by the list variable 
+  list.append(node);
 }
+const list = document.querySelector('.todo-list');
+// Adding a click event listener to the list and its children will
+list.addEventListener('click', event => {
+  if (event.target.classList.contains('tick')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+})
