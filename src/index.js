f520c95e702f130bce079e3231126a1f25ba3fd7
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-14 14:56:46 +0200

    Iterate through the list

--- src/index.js
+++ src/index.js
@@ -1,40 +1,23 @@
-// Selectors for
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
+// declarations
+const form = document.querySelector('#itemForm'); //select the form
+const itemInput = document.querySelector('#itemInput'); // select the input box from the form
+const itemList = document.querySelector('.item-list');
+const feedback = document.querySelector('.feedback');
+const clearButton = document.querySelector('#clear-list');
 
-// Events Listeners 
+let todoItems = [];
 
-todoButton.addEventListener('click', addTodo);
+const items = document.querySelectorAll('.item-list');
 
-// Functions 
-
-function addTodo(event) {
-  // lets prevent a default
-  event.preventDefault();
-  // Lets create Todo DIV 
-  const todoDiv = document.createElement('div');
-  todoDiv.classList.add("todo");
-
-  // create the List 
-  const newTodo = document.createElement('li');
-  newTodo.innerText = 'hey';
-  newTodo.classList.add('todo-item');
-  todoDiv.appendChild(newTodo);
-
-  // check box or button
-
-  const completedButton = document.createElement('button');
-  completedButton.innerHTML = '<i class="fas fa-check"></i>'
-  completedButton.classList.add('complete-btn');
-  todoDiv.appendChild(completedButton);
-
-  //check delete button or button
-  const trashButton = document.createElement('button');
-  trashButton.innerHTML = '<i class="fas fa-trash"></i>'
-  trashButton.classList.add('complete-btn');
-  todoDiv.appendChild(trashButton);
-
-  // Append to list
-  todoList.appendChild(todoDiv);
+const addTodo = function(itemName) {
+  items.forEach(function(item) {
+    
+    if(item.querySelector('.item-name').textContent === itemName) {
+      // event listener for complete
+      item.querySelector('.complete-item').addEventListener('click', function() {
+        item.querySelector('.item-name').classList.toggle('completed');
+        this.classList.toggle('visibility');
+      });
+    }
+  })
 }
