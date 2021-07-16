294120f2d59fa21e5fa418ce6998bc7c54669c42
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-16 16:52:49 +0200

    Finish the add and remove metods

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable func-names */
-// eslint-disable-next-line no-unused-vars
 import css from './style.css';
 import html from './index.html';
 // declarations
@@ -12,111 +11,96 @@ const clearButton = document.querySelector('#clear-list');
 
 let todoItems = [];
 
-const items = document.querySelectorAll('.item-list');
+const saveAndRender = function () {
+  // save to storage
+  localStorage.setItem('todoItems', JSON.stringify(todoItems));
 
-const getList = function (todoItems) {
+  // clear items HTML
   itemList.innerHTML = '';
 
-  todoItems.forEach((item) => {
+  // generate items HTML
+  todoItems.forEach((todoItem) => {
+    const completed = todoItem.checked ? 'completed' : '';
     itemList.insertAdjacentHTML('beforeend',
-      `<div class="item my-3">
-  <h5 class="item-name text-capitalize">${item}</h5>
-  <div class="item-icons"><a href="#" class="complete-item mx-2 item-icon">
-  <i class="far fa-check-circle"></i></a><a href="#" class="edit-item mx-2 item-icon">
-  <i class="far fa-edit"></i></a><a href="#" class="delete-item item-icon">
-  <i class="far fa-times-circle"></i></a></div></div>`);
-
-    // eslint-disable-next-line no-use-before-define
-    addTodo(item);
+      ` <div class="item my-3" draggable="true" id="${todoItem.id}">
+        <h5 class="item-name text-capitalize ${completed}">${todoItem.text}</h5>
+        <div class="item-icons">
+          <i class="far fa-check-circle complete-item mx-2 item-icon"></i>
+          <i class="far fa-times-circle delete-item item-icon"></i>
+        </div>
+      </div>`);
   });
 };
 
-const removeItem = function (item) {
-  // console.log(item);
-  const removeIndex = (todoItems.indexOf(item));
-  // console.log(removeIndex);
+const completeItem = function (itemId) {
+  const todoItem = todoItems.find((tdi) => tdi.id === itemId);
+  todoItem.checked = !todoItem.checked;
+  saveAndRender();
+};
+
+const removeItem = function (itemId) {
+  const todoItem = todoItems.find((tdi) => tdi.id === itemId);
+  const removeIndex = (todoItems.indexOf(todoItem));
   todoItems.splice(removeIndex, 1);
-  getList(todoItems);
+  saveAndRender();
 };
 
-const addTodo = function (itemName) {
-  todoItems.push({
-    text: itemName,
-    checked: false,
-    id: Date.now(),
-  });
-  items.forEach((item) => {
-    if (item.querySelector('.item-name').textContent === itemName) {
-      // event listener for complete
-      item.querySelector('.complete-item').addEventListener('click', function () {
-        item.querySelector('.item-name').classList.toggle('completed');
-        this.classList.toggle('visibility');
-      });
-      // event listener for for edit
-      item.querySelector('.edit-item').addEventListener('click', () => {
-        itemInput.value = itemName;
-        itemList.removeChild(item);
-
-        todoItems = todoItems.filter((item) => item !== itemName);
-      });
-      // event listener for delete
-      item.querySelector('.delete-item').addEventListener('click', () => {
-        // itemList.removeChild(item);
-
-        const dataItem = todoItems.find((item) => item === itemName);
-        removeItem(dataItem);
-        getList(todoItems);
-        // showinform('item delete', 'success')
-      });
-    }
-  });
-};
-
-// get local storage
-const getLocalStorage = function () {
-  const todoStorage = localStorage.getItem('todoItems');
-  if (todoStorage === 'undefined' || todoStorage === null) {
-    todoItems = [];
-  } else {
-    todoItems = JSON.parse(todoStorage);
-    getList(todoItems);
-  }
-};
-
-// set local storage
-const setLocalStorage = function (todoItems) {
-  localStorage.setItem('todoItems', JSON.stringify(todoItems));
-};
-
-// get local storage
-getLocalStorage();
-
-// add a list item and to local storage
+// event handler to add a list item
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const itemName = itemInput.value;
 
-  if (itemName === 0) {
+  // collect input asnd clear textbox
+  const itemText = itemInput.value;
+  itemInput.value = '';
+
+  // validate
+  if (!itemText || !itemText.trim()) {
     inform.innerHTML = 'Enter a Valid to do';
     inform.classList.add('showItem', 'alert-danger');
     setTimeout(() => {
       inform.classList.remove('showItem');
     }, 3000);
-  } else {
-    todoItems.push(itemName);
-    setLocalStorage(todoItems);
-    getList(todoItems);
-    // add event listeners to icons;
-    // addtodo(itemName);
+    return;
   }
 
-  itemInput.value = '';
+  // add to item
+  todoItems.push({
+    text: itemText,
+    checked: false,
+    id: new Date().getTime(),
+  });
+
+  // update UI
+  saveAndRender();
 });
 
-// clear items from the listeners
-
-clearButton.addEventListener('click', () => {
+// event handler to clear items
+clearButton.addEventListener('click', (e) => {
+  e.preventDefault();
   todoItems = [];
-  localStorage.clear();
-  getList(todoItems);
+  saveAndRender();
 });
+
+// event handler to complete, edit and delete
+itemList.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  // decide which of our action button were clicked (if any)
+  const targetClasses = e.target.classList;
+  const getItemId = () => Number(e.target.closest('div.item').id);
+
+  if (targetClasses.contains('complete-item')) {
+    completeItem(getItemId());
+  } else if (targetClasses.contains('delete-item')) {
+    removeItem(getItemId());
+  }
+
+  // saveAndRender();
+});
+
+// retrieve to items from storage
+const storedTodoItems = localStorage.getItem('todoItems') || '[]';
+todoItems = JSON.parse(storedTodoItems);
+saveAndRender();
+
+// draggable
