de39e255f5c2e5bf43e8599cb10da3f38d86921e
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-27 21:27:09 +0200

    caal the locakl storage mock

--- src/ItemRepository.test.js
+++ src/ItemRepository.test.js
@@ -1,2 +1,8 @@
 import 'jest-localstorage-mock';
 import ItemRepository from './ItemRepository.js';
+
+beforeEach(() => {
+  localStorage.clear();
+  jest.clearAllMocks();
+  localStorage.setItem.mockClear();
+});
