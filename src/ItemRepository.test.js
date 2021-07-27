52c61e5cc83c309504fa59e59da9d7143bdaacc3
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-27 21:28:36 +0200

    Let add the test to make sure the todo list starts from zero

--- src/ItemRepository.test.js
+++ src/ItemRepository.test.js
@@ -6,3 +6,13 @@ beforeEach(() => {
   jest.clearAllMocks();
   localStorage.setItem.mockClear();
 });
+
+describe('ItemRepository', () => {
+  it('should start with zero items', () => {
+    // arrange
+    const itemRepository = new ItemRepository();
+
+    // assert
+    expect(itemRepository.todoItems.length).toBe(0);
+  });
+});
