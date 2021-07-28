d7e776df44f731901e1647459dad2219f1c0e7f4
adewunmi <shonibareadewunmicomfort@gmail.com>
2021-07-28 14:22:30 +0100

    add addItem test

--- src/ItemRepository.test.js
+++ src/ItemRepository.test.js
@@ -16,3 +16,25 @@ describe('ItemRepository', () => {
     expect(itemRepository.todoItems.length).toBe(0);
   });
 });
+
+describe('ItemRepository.addItem', () => {
+  it('should return a string ID', () => {
+    // arrange
+    const itemRepository = new ItemRepository();
+    const ID1 = itemRepository.addItem('one');
+
+    // assert
+    expect(ID1).toBeTruthy();
+    expect(typeof ID1).toBe('string');
+    expect(ID1).toBe(ID1.trim());
+
+    const ID2 = itemRepository.addItem('two');
+
+    // assert
+    expect(ID2).toBeTruthy();
+    expect(typeof ID2).toBe('string');
+    expect(ID2).toBe(ID2.trim());
+
+    expect (ID2).not.toBe(ID1);
+  });
+});
