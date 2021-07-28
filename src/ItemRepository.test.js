a73a3ee5658f73f1b7cb2812f7db6ec85005af44
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-28 14:28:10 +0200

    Test the id return

--- src/ItemRepository.test.js
+++ src/ItemRepository.test.js
@@ -16,3 +16,26 @@ describe('ItemRepository', () => {
     expect(itemRepository.todoItems.length).toBe(0);
   });
 });
+describe('ItemRepository.addItem', () => {
+  it('should return a string ID', () => {
+    // arrange
+    const itemRepository = new ItemRepository();
+
+    // act
+    const id1 = itemRepository.addItem('Hello');
+
+    // assert
+    expect(id1).toBeTruthy();
+    expect(typeof id1).toBe('string');
+    expect(id1).toBe(id1.trim());
+
+    // act (again)
+    const id2 = itemRepository.addItem('You');
+
+    // assert (again)
+    expect(id2).toBeTruthy();
+    expect(typeof id2).toBe('string');
+    expect(id2).toBe(id2.trim());
+
+    expect(id2).not.toBe(id1); // make sure different ID is generated
+  });
